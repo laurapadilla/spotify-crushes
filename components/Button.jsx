4876204d47325f8e4c9121e3d40b6b494adc7345
laurapadilla <hi@lauracodes.com>
2022-11-14 22:15:33 -0500

    lettuce try agrain 🥬

--- components/Button.jsx
+++ components/Button.jsx
@@ -7,49 +7,90 @@ export default function Button({ fetchUser }) {
   const scopes = `user-top-read`;
   const AUTHORIZATION_URL = `https://accounts.spotify.com/authorize?client_id=${client_id}&response_type=token&redirect_uri=${redirect_uri}&scope=${scopes}&show_dialog=true`;
 
-  const login = () => {
-    let popup = window.open(
-      AUTHORIZATION_URL,
-      "Login with Spotify",
-      "width=800,height=600"
-    );
-    window.spotifyCallback = (payload) => {
-      console.log("there");
-      popup.close();
-      fetch("https://api.spotify.com/v1/me/top/tracks?time_range=medium_term", {
-        method: "GET",
-        headers: {
-          Authorization: `Bearer ${payload}`,
-        },
-      })
+  // const login = () => {
+  //   let popup = window.open(
+  //     AUTHORIZATION_URL,
+  //     "Login with Spotify",
+  //     "width=800,height=600"
+  //   );
+  //   window.spotifyCallback = (payload) => {
+  //     console.log("there");
+  //     popup.close();
+  //     fetch("https://api.spotify.com/v1/me/top/tracks?time_range=medium_term", {
+  //       method: "GET",
+  //       headers: {
+  //         Authorization: `Bearer ${payload}`,
+  //       },
+  //     })
+  //       .then((response) => {
+  //         return response.json();
+  //       })
+  //       .then((data) => {
+  //         const { items } = data;
+  //         showButton(false);
+  //         const tracks = items.map((track) => ({
+  //           artist: track.artists.map((_artist) => _artist.name).join(", "),
+  //           songUrl: track.external_urls.spotify,
+  //           title: track.name,
+  //         }));
+  //         console.log(items);
+  //         console.log("hi");
+  //         fetchUser({ tracks });
+  //       });
+  //   };
+  // };
+
+  const [token, setToken] = useState("");
+  const [tracks, setTracks] = useState([]);
+
+  useEffect(() => {
+    const hash = window.location.hash;
+    let token = window.localStorage.getItem("token");
+
+    if (!token && hash) {
+      token = hash
+        .substring(1)
+        .split("&")
+        .find((elem) => elem.startsWith("access_token"))
+        .split("=")[1];
+
+      window.location.hash = "";
+      window.localStorage.setItem("token", token);
+    }
+
+    setToken(token);
+
+    if (token) {
+      fetch(
+        "https://api.spotify.com/v1/me/top/tracks?time_range=medium_term&limit=50&offset=0",
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      )
         .then((response) => {
           return response.json();
         })
         .then((data) => {
           const { items } = data;
-          showButton(false);
           const tracks = items.map((track) => ({
+            id: track.id,
+            album: track.album.name,
+            albumImg: track.album.images[0],
             artist: track.artists.map((_artist) => _artist.name).join(", "),
             songUrl: track.external_urls.spotify,
             title: track.name,
           }));
-          console.log(items);
-          console.log("hi");
-          fetchUser({ tracks });
+          setTracks(tracks);
+          console.log(tracks);
         });
-    };
-  };
-
-  useEffect(() => {
-    const token = window.location.hash.substr(1).split("&")[0].split("=")[1];
-    if (token) {
-      window.opener.spotifyCallback(token);
     }
   }, []);
 
   return (
     <>
-      {button && <button onClick={login}>Login to see your top songs!</button>}
+      {button && <a href={AUTHORIZATION_URL}>Login to see your top songs!</a>}
     </>
   );
 }
