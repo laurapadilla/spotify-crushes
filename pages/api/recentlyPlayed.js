3071b12bfec2e3ccb301f3d6df0e2dcd2dc81730
laurapadilla <hi@lauracodes.com>
2022-11-14 21:35:19 -0500

    pppfffffff

--- pages/api/recentlyPlayed.js
+++ pages/api/recentlyPlayed.js
@@ -5,7 +5,7 @@ export default async (_, res) => {
   const response = await getTopTracks();
   const { items } = await response.json();
 
-  const tracks = items.slice(0, 10).map((track) => ({
+  const tracks = items.map((track) => ({
     artist: track.artists.map((_artist) => _artist.name).join(", "),
     songUrl: track.external_urls.spotify,
     title: track.name,
@@ -13,11 +13,5 @@ export default async (_, res) => {
 
   console.log(tracks);
 
-  return new Response(JSON.stringify({ tracks }), {
-    status: 200,
-    headers: {
-      "content-type": "application/json",
-      "cache-control": "public, s-maxage=86400, stale-while-revalidate=43200",
-    },
-  });
+  return res.status(200).json({ tracks });
 };
