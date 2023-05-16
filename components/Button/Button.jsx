477ede1445b3beaa4be20bb0e77a847b5db43f7e
Laura Padilla <hi@lauracodes.com>
2023-05-16 15:53:13 -0400

    let see

--- components/Button/Button.jsx
+++ components/Button/Button.jsx
@@ -162,7 +162,7 @@ export function Button({ fetchUser }) {
 
   return (
     <>
-      {button ? (
+      {!token ? (
         <LoginButton as="button" onClick={login}>
           <Text fontFamily="sansMedium" size={2}>
             Login with Spotify to see your Top 40!
