17ccf936020c3363da53a32fa1c33b758c85bdd6
Laura Padilla <hi@lauracodes.com>
2023-05-16 16:50:34 -0400

    revert

--- components/Button/Button.jsx
+++ components/Button/Button.jsx
@@ -4,7 +4,7 @@ import { LoginButton } from "./styles";
 
 export function Button({ fetchUser }) {
   const [button, showButton] = useState(true);
-  const [token, setToken] = useState("");
+  // const [token, setToken] = useState("");
   const client_id = `ad2c7654ff92405c949de032535da426`;
   const redirect_uri = `https://www.top40fm.xyz/`;
   const scopes = `user-top-read`;
@@ -107,34 +107,34 @@ export function Button({ fetchUser }) {
       });
   };
 
-  // useEffect(() => {
-  //   const token = window.location.hash.substr(1).split("&")[0].split("=")[1];
-  //   if (token) {
-  //     window.opener.spotifyCallback(token);
-  //   }
-  // }, []);
-
   useEffect(() => {
-    const hash = window.location.hash;
-    let token = window.localStorage.getItem("token");
-
-    if (!token && hash) {
-      token = hash
-        .substring(1)
-        .split("&")[0]
-        .find((elem) => elem.startsWith("access_token"))
-        .split("=")[1];
-
-      window.location.hash = "";
-      window.localStorage.setItem("token", token);
+    const token = window.location.hash.substr(1).split("&")[0].split("=")[1];
+    if (token) {
+      window.opener.spotifyCallback(token);
     }
-    setToken(token);
   }, []);
 
-  const logout = () => {
-    setToken("");
-    window.localStorage.removeItem("token");
-  };
+  // useEffect(() => {
+  //   const hash = window.location.hash;
+  //   let token = window.localStorage.getItem("token");
+
+  //   if (!token && hash) {
+  //     token = hash
+  //       .substr(1)
+  //       .split("&")[0]
+  //       .find((elem) => elem.startsWith("access_token"))
+  //       .split("=")[1];
+
+  //     window.location.hash = "";
+  //     window.localStorage.setItem("token", token);
+  //   }
+  //   setToken(token);
+  // }, []);
+
+  // const logout = () => {
+  //   setToken("");
+  //   // window.localStorage.removeItem("token");
+  // };
 
   return (
     <>
@@ -144,9 +144,7 @@ export function Button({ fetchUser }) {
             Login with Spotify to see your Top 40!
           </Text>
         </LoginButton>
-      ) : (
-        <button onClick={logout}>logout</button>
-      )}
+      ) : null}
     </>
   );
 }
