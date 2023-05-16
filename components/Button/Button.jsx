d334b7641a459f88cbd41313006ce86628320dd3
Laura Padilla <hi@lauracodes.com>
2023-05-16 16:26:04 -0400

    work pls pls

--- components/Button/Button.jsx
+++ components/Button/Button.jsx
@@ -121,14 +121,13 @@ export function Button({ fetchUser }) {
     if (!token && hash) {
       token = hash
         .substring(1)
-        .split("&")
+        .split("&")[0]
         .find((elem) => elem.startsWith("access_token"))
         .split("=")[1];
 
       window.location.hash = "";
       window.localStorage.setItem("token", token);
     }
-    window.opener.spotifyCallback(token);
     setToken(token);
   }, []);
 
