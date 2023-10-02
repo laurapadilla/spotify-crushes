ddfd2cc349133a0f7c1156223fd89ea43db0e95c
Laura Padilla <hi@lauracodes.com>
2023-10-02 11:51:29 -0400

    does it work

--- components/Button/Button.jsx
+++ components/Button/Button.jsx
@@ -4,13 +4,10 @@ import { LoginButton } from "./styles";
 
 export function Button({ fetchUser }) {
   const [button, showButton] = useState(true);
-  // const [token, setToken] = useState("");
   const client_id = `ad2c7654ff92405c949de032535da426`;
   const redirect_uri = `https://www.top40fm.xyz/`;
   const scopes = `user-top-read`;
   const AUTHORIZATION_URL = `https://accounts.spotify.com/authorize?client_id=${client_id}&response_type=token&redirect_uri=${redirect_uri}&scope=${scopes}&show_dialog=true`;
-
-  // METHOD 1
 
   const login = () => {
     let popup = window.open(
@@ -62,144 +59,42 @@ export function Button({ fetchUser }) {
     }
   }, []);
 
-  // METHOD 2
+  const revokeToken = async (refreshToken) => {
+    const clientId = process.env.SPOTIFY_CLIENT_ID;
+    const clientSecret = process.env.SPOTIFY_CLIENT_SECRET;
 
-  // const login = () => {
-  //   let popup = window.open(
-  //     AUTHORIZATION_URL,
-  //     "Login with Spotify",
-  //     "width=800,height=600"
-  //   );
-  //   window.spotifyCallback = (payload) => {
-  //     console.log("there");
-  //     popup.close();
-  //     getTracks(payload);
-  //   };
-  // };
+    try {
+      const response = await fetch("https://accounts.spotify.com/api/token", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+          Authorization: `Basic ${btoa(`${clientId}:${clientSecret}`)}`,
+        },
+        body: `token=${refreshToken}&token_type_hint=refresh_token`,
+      });
 
-  // const getTracks = async (payload) => {
-  //   fetch(
-  //     "https://api.spotify.com/v1/me/top/tracks?time_range=medium_term&limit=40&offset=0",
-  //     {
-  //       method: "GET",
-  //       headers: {
-  //         Authorization: `Bearer ${payload}`,
-  //       },
-  //     }
-  //   )
-  //     .then((response) => {
-  //       return response.json();
-  //     })
-  //     .then((data) => {
-  //       const { items } = data;
-  //       showButton(false);
-  //       const tracks = items.map((track) => ({
-  //         id: track.id,
-  //         album: track.album.name,
-  //         albumURL: track.album.external_urls.spotify,
-  //         albumImg: track.album.images[0],
-  //         artist: track.artists.map((_artist) => _artist.name).join(", "),
-  //         artistURL: track.artists
-  //           .map((_artist) => _artist.external_urls.spotify)
-  //           .join(", "),
-  //         songUrl: track.external_urls.spotify,
-  //         title: track.name,
-  //       }));
-  //       console.log(items);
-  //       console.log("logged in");
-  //       fetchUser({ tracks });
-  //     });
-  // };
+      if (response.ok) {
+        return true; // Token successfully revoked
+      }
+    } catch (error) {
+      console.error("Error revoking token:", error);
+    }
 
-  // useEffect(() => {
-  //   const token = window.location.hash.substr(1).split("&")[0].split("=")[1];
-  //   if (token) {
-  //     window.opener.spotifyCallback(token);
-  //   }
-  // }, []);
+    return false; // Token revocation failed
+  };
 
-  // useEffect(() => {
-  //   const hash = window.location.hash;
-  //   let token = window.localStorage.getItem("token");
+  const disconnectFromSpotify = async () => {
+    const refreshToken = process.env.SPOTIFY_REFRESH_TOKEN; // Replace with the user's refresh token
 
-  //   if (!token && hash) {
-  //     token = hash
-  //       .substr(1)
-  //       .split("&")[0]
-  //       .find((elem) => elem.startsWith("access_token"))
-  //       .split("=")[1];
+    const success = await revokeToken(refreshToken);
 
-  //     window.location.hash = "";
-  //     window.localStorage.setItem("token", token);
-  //   }
-  //   setToken(token);
-  // }, []);
-
-  // const logout = () => {
-  //   setToken("");
-  //   // window.localStorage.removeItem("token");
-  // };
-
-  // METHOD 3
-  // const login = () => {
-  //   let popup = window.open(
-  //     AUTHORIZATION_URL,
-  //     "Login with Spotify",
-  //     "width=800,height=600"
-  //   );
-  //   window.spotifyCallback = () => {
-  //     console.log("there");
-  //     popup.close();
-
-  //     const token = window.location.hash.substr(1).split("&")[0].split("=")[1];
-  //     if (token) {
-  //       window.opener.spotifyCallback(token);
-  //     }
-  //   };
-  // };
-
-  // const getTracks = useCallback(() => {
-  //   if (token) {
-  //     fetch(
-  //       "https://api.spotify.com/v1/me/top/tracks?time_range=medium_term&limit=40&offset=0",
-  //       {
-  //         method: "GET",
-  //         headers: {
-  //           Authorization: `Bearer ${token}`,
-  //         },
-  //       }
-  //     )
-  //       .then((response) => {
-  //         return response.json();
-  //       })
-  //       .then((data) => {
-  //         const { items } = data;
-  //         showButton(false);
-  //         const tracks = items.map((track) => ({
-  //           id: track.id,
-  //           album: track.album.name,
-  //           albumURL: track.album.external_urls.spotify,
-  //           albumImg: track.album.images[0],
-  //           artist: track.artists.map((_artist) => _artist.name).join(", "),
-  //           artistURL: track.artists
-  //             .map((_artist) => _artist.external_urls.spotify)
-  //             .join(", "),
-  //           songUrl: track.external_urls.spotify,
-  //           title: track.name,
-  //         }));
-  //         console.log(items);
-  //         console.log("hi");
-  //         fetchUser({ tracks });
-  //       });
-  //   } else {
-  //     // Logged out view logic
-  //     console.log("logged out");
-  //   }
-  // }, [token, fetchUser]);
-
-  // useEffect(() => {
-  //   getTracks();
-  // }, [getTracks]);
+    if (success) {
+      // Perform any necessary cleanup on your application's side
+      alert("Successfully disconnected from Spotify");
+    } else {
+      alert("Failed to disconnect from Spotify");
+    }
+  };
 
   return (
     <>
